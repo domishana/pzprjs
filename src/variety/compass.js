f7c0365ef7084bf86d9432b168b3a00c7606372c
domishana <shana3698shana+dominion@gmail.com>
2019-11-08 06:32:56 +0000

    modify error msgs for compass.

--- src/variety/compass.js
+++ src/variety/compass.js
@@ -241,7 +241,7 @@ AnsCheck:{
 },
 
 FailCode:{
-	ceDirection : ["数字の方向にあるブロック内のマス数が、数字と異なっているブロックがあります。","The number of cells in the direction is wrong."],
+	ceDirection : ["数字の方向にあるブロック内のマス数が、数字と一致していないブロックがあります。","The number of cells in the direction is wrong."],
 	ceNoNum     : ["記号マスを１つも含まないブロックがあります。","There is an area without a clue."],
 	ceDoubleNum : ["２つ以上の記号マスを含むブロックがあります。","There is an area with more than one clue."]
 }
